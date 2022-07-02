0cb94dba643826fd195052f3c7e33b4598ab6353
Mark Pavel <markpavel02@gmail.com>
2022-07-02 14:57:25 -0400

    Homepage Functionality

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,53 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage')
+    Post.findAll({
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        return res.render('homepage', {
+            posts: posts,
+            notHome: false,
+            notDash: true,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    
+    res.render('login', { notHome: true });
 });
 
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes');
+const dashRoutes = require('./dash-routes');
 
 router.use('/', homeRoutes);
+router.use('/dashboard', dashRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+});
 
 module.exports = router;
