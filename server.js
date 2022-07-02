80c7f1621d048016eeb91f4210ca45fccaedc810
Mark Pavel <markpavel02@gmail.com>
2022-07-01 20:44:28 -0400

    Homepage Rendering

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const app = express();
+
+const routes = require('./controllers');
+app.use(routes);
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+app.use(session(sess));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening to: ${PORT}`));
+});
